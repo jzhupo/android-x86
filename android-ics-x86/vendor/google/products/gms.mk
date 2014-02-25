bda4517de33f1767037adee495a810e95fa2be59
Hu Po <po.hu@qq.com>
2014-02-26 07:39:14 +0800

    GMS package

--- android-ics-x86/vendor/google/products/gms.mk
+++ android-ics-x86/vendor/google/products/gms.mk
@@ -1,4 +1,4 @@
 #LOCAL_PATH := vendor/google/products
-LOCAL_SRC_FILES := $(filter-out %.apk,$(subst $(LOCAL_PATH)/,,$(shell find $(LOCAL_PATH)/system -type f)))
+LOCAL_SRC_FILES := $(filter-out %$(COMMON_ANDROID_PACKAGE_SUFFIX),$(subst $(LOCAL_PATH)/,,$(shell find $(LOCAL_PATH)/system -type f)))
 PRODUCT_COPY_FILES += $(foreach f,$(LOCAL_SRC_FILES),$(LOCAL_PATH)/$(f):$(f))
 
