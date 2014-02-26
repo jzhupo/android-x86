7338271909bac0d6a3cdea21c1aacd101748994c
Hu Po <po.hu@qq.com>
2014-02-27 00:42:39 +0800

    Revert product configuration

--- android-ics-x86/device/hupo/impc/impc.mk
+++ android-ics-x86/device/hupo/impc/impc.mk
@@ -3,6 +3,7 @@ PRODUCT_PACKAGES += sensors.$(TARGET_PRODUCT)
 PRODUCT_PACKAGES += audio.primary.$(TARGET_PRODUCT) audio_policy.$(TARGET_PRODUCT) audio.a2dp.default alsa.$(TARGET_PRODUCT)
 PRODUCT_PACKAGES += AndroidTerm FileManager
 PRODUCT_PACKAGES += PinyinIME dhcpcd.conf
+PRODUCT_PACKAGES += SpeechRecorder NoiseField Stk RSSReader
 
 PRODUCT_COPY_FILES := \
 	frameworks/base/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
@@ -12,13 +13,7 @@ PRODUCT_COPY_FILES := \
 
 ifeq (yes, no)
 PRODUCT_COPY_FILES += \
-        $(LOCAL_PATH)/driver/realtek/wlan.ko:system/lib/modules/wlan.ko \
-        $(LOCAL_PATH)/driver/3dsp/bus/3dspusbbus.ko:system/lib/modules/3dspusbbus.ko \
-        $(LOCAL_PATH)/driver/3dsp/private/wlan_priv/3dspusbwlanpriv.ko:system/lib/modules/3dspusbwlanpriv.ko \
-        $(LOCAL_PATH)/driver/3dsp/wlan/3dspusbwlan.ko:system/lib/modules/3dspusbwlan.ko \
-        $(LOCAL_PATH)/driver/3dsp/private/new_bluetooth_priv/3dspusbbtpriv.ko:system/lib/modules/3dspusbbtpriv.ko \
-        $(LOCAL_PATH)/driver/3dsp/new_bluetooth/3dspusbbt.ko:system/lib/modules/3dspusbbt.ko \
-        $(LOCAL_PATH)/driver/3dsp/3dspcode.bin:system/lib/modules/3dspcode.bin
+	$(LOCAL_PATH)/driver/realtek/wlan.ko:system/lib/modules/wlan.ko
 endif
 
 PRODUCT_PROPERTY_OVERRIDES := \
