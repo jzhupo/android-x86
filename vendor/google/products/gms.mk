0f6226818631b6404d957762d9a0c49adfe7edd3
Hu Po <po.hu@qq.com>
2014-03-29 10:45:30 +0800

    Update GMS package

--- vendor/google/products/gms.mk
+++ vendor/google/products/gms.mk
@@ -1,4 +1,7 @@
 LOCAL_SRC_FILES := $(filter-out %$(COMMON_ANDROID_PACKAGE_SUFFIX),$(subst $(LOCAL_PATH)/,,$(shell find $(LOCAL_PATH)/system -type f)))
 PRODUCT_COPY_FILES += $(foreach f,$(LOCAL_SRC_FILES),$(LOCAL_PATH)/$(f):$(f))
-PRODUCT_PROPERTY_OVERRIDES += ro.com.google.gmsversion=4.4.2_r1
-
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.addon.type=gapps \
+	ro.addon.platform=kk \
+	ro.addon.version=gapps-kk-20140105 \
+	ro.addon.minimumversion=4.4.2
